edf6ae965abda6f5760880b7e0d752c6a8e5ded4
Dan Haski <6283385+mrbamba@users.noreply.github.com>
2020-06-24 20:57:16 +0300

    more progress

--- js/apps/email/cmps/email-list.cmp.js
+++ js/apps/email/cmps/email-list.cmp.js
@@ -1,10 +1,15 @@
-export default{
-    prop:['emails'],
+import emailPreview from './email-preview.cmp.js'
+
+export default {
+    props:['emails'],
     template:`
     <div class="email-list">
         <table>
             <email-preview v-for="email in emails" @click.native="selectEmail(email)" :email="email" :key="email.id"/>
         </table>
     </div>
-    `
+    `,
+    components:{
+        emailPreview
+    }
 }
