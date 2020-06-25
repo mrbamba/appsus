7a2cafca81a8d88d5d56eaea621e9eee6c4c95b8
Samantha Kofler <samantha_kofler@outlook.com>
2020-06-25 14:20:17 +0300

    editor toggle

--- js/apps/notes/cmps/note-text.cmp.js
+++ js/apps/notes/cmps/note-text.cmp.js
@@ -1,7 +1,39 @@
 export default {
     props:['info'],
     template: `
-        <div> {{info.txt}} </div>
-    `
+        <div class="note" :style="{backgroundColor: bgc}" > 
+        <i class="fas fa-thumbtack"></i>
+        <p>{{info.txt}}</p>
+        <i class="fas fa-palette icon-color"></i>
+        <input type="color" class="color" @blur="onSetFillColor(value)">
+        <i class="fas fa-trash-alt"></i>
+         </div>
+    `,
+    data() {
+        return {
+            bgc: ''
+        }
+    },
+    methods: {
+        changeBgc(ev) {
+            const color = ev.target.value;
+            this.bgc = color;
+ 
+         }
+    }
 }
 
+
+// <select id="colorPickerBackground" @change="changeBgc($event)">
+
+// <option value="transparent">None</option>
+// <option value="yellow">Yellow</option>
+// <option value="salmon">Salmon</option>
+// <option value="lightblue">Light Blue</option>
+// <option value="limegreen">Lime Green</option>
+// <option value="cyan">Cyan</option>
+// <option value="violet">Violet</option>
+// <option value="red">Red</option>
+// </select>
+
+/* <input type="color" class="color" @blur="onSetFillColor(value)"> */
--- js/apps/notes/cmps/note-todos.cmp.js
+++ js/apps/notes/cmps/note-todos.cmp.js
@@ -5,13 +5,31 @@ import { noteService } from '../services/notes.service.js';
 export default {
     props: ['info'],
     template: `
-        <section>
+        <div class="note" :style="{backgroundColor: bgc}" > 
+        <i class="fas fa-thumbtack"></i>
         <ul class="todo-list"> {{info.label}}
         <li class="todo" v-for="todo in info.todos" :class="{done: !todo.doneAt}" @click="todo.doneAt = !todo.doneAt">{{todo.txt}}</li>
         </ul>
-        </section>
-    `,
+        <i class="fas fa-palette"></i>
+        <select id="colorPickerBackground" @change="changeBgc($event)">
 
+        <option value="transparent">None</option>
+        <option value="yellow">Yellow</option>
+        <option value="salmon">Salmon</option>
+        <option value="lightblue">Light Blue</option>
+        <option value="limegreen">Lime Green</option>
+        <option value="cyan">Cyan</option>
+        <option value="violet">Violet</option>
+        <option value="red">Red</option>
+        </select>
+        <i class="fas fa-trash-alt"></i>
+        </div>
+    `,
+    data() {
+        return {
+            bgc: ''
+        }
+    },  
     // data() {
     //     // return {
     //     //     isDone:
@@ -32,7 +50,12 @@ export default {
             else this.isDone = false;
             // noteService.toggleDoneStatus()
             console.log('toggle',this.isDone)
-        }
+        },
+        changeBgc(ev) {
+            const color = ev.target.value;
+            this.bgc = color;
+ 
+         }
     },
     components: {
         noteService
