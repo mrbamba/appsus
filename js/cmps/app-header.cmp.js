4cfe2364fc755fbc7febf09524d5c20ae35238bc
Dan Haski <6283385+mrbamba@users.noreply.github.com>
2020-06-25 14:22:51 +0300

    fixed home page always being shown as selected

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -3,7 +3,7 @@ export default {
      <header class="app-header ">
         <input class="main-search" placeholder="Search"/>
         <nav class="main-nav">
-                <router-link to="/"><i class="fas fa-igloo"></i></router-link>
+                <router-link to="/" exact><i class="fas fa-igloo"></i></router-link>
                 <router-link to="/email"><i class="far fa-envelope"></i></router-link>
                 <router-link to="/notes"><i class="far fa-sticky-note"></i></i></router-link>
                 <router-link to="/books"><i class="fas fa-book"></i></router-link>
