b6f557462449a63333868659419cbd0d70d29eda
Dan Haski <6283385+mrbamba@users.noreply.github.com>
2020-06-26 13:44:35 +0300

    minor

--- js/apps/email/cmps/email-nav-bar.cmp.js
+++ js/apps/email/cmps/email-nav-bar.cmp.js
@@ -8,12 +8,12 @@ export default{
     <div class="mail-nav-bar">
     <button class="compose-button" v-on:click="compose"><i class="fa fa-plus"></i>  Compose</button>
     <ul class="email-nav-list">
-        <li><router-link to="/email/inbox">Inbox</router-link><email-status v-bind:emails="emails"/></li>
-        <li><router-link to="/email/starred">Starred</router-link></li>
-        <li><router-link to="/email/sent">Sent</router-link></li>
-        <li><router-link to="/email/spam">Spam</router-link></li>
-        <li><router-link to="/email/trash">Trash</router-link></li>
-        <li><router-link to="/email/all">All Emails</router-link></li>
+        <li><router-link class="router-link" to="/email/inbox">Inbox</router-link><email-status v-bind:emails="emails"/></li>
+        <li><router-link class="router-link" to="/email/starred">Starred</router-link></li>
+        <li><router-link class="router-link" to="/email/sent">Sent</router-link></li>
+        <li><router-link class="router-link" to="/email/spam">Spam</router-link></li>
+        <li><router-link class="router-link" to="/email/trash">Trash</router-link></li>
+        <li><router-link class="router-link" to="/email/all">All</router-link></li>
 
     </ul>
     </div>
