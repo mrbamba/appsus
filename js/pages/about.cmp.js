5f036e331f2490f0c634a9eba8389ef5b275000a
Dan Haski <6283385+mrbamba@users.noreply.github.com>
2020-06-27 21:40:36 +0300

    trying to fix images

--- js/pages/about.cmp.js
+++ js/pages/about.cmp.js
@@ -6,14 +6,14 @@ export default {
             <h2>Our Team</h2>
             <div class="flex flex-row  justify-center">
                 <div  class="about about-team">
-                        <img src="img/about/dan.jpg" class="profile-img">
+                        <img src="./img/about/dan.jpg" class="profile-img">
                         <div class="about-text">
                         <p class="name-about"> Dan Haski </p>
                         <p> </p>
                         </div>
                 </div>
                 <div  class="about about-team">
-                <img src="img/about/sama-profile.jpg"  class="profile-img">
+                <img src="./img/about/sama-profile.jpg"  class="profile-img">
                 <div class="about-text">
                 <p class="name-about"> Samantha Kofler </p>
                 <p> </p>
