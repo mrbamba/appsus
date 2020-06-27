debba2751809c0a30bf18708961a3759e27bec9e
Dan Haski <6283385+mrbamba@users.noreply.github.com>
2020-06-27 22:02:05 +0300

    fixed about responsiveness

--- js/pages/about.cmp.js
+++ js/pages/about.cmp.js
@@ -4,21 +4,19 @@ export default {
     template: `
 <section class="about">
             <h2>Our Team</h2>
-            <div class="flex flex-row  justify-center">
+            <div class="about-personel ">
                 <div  class="about about-team">
-                        <img src="./img/about/dan.jpg" class="profile-img">
-                        <div class="about-text">
+                    <img src="./img/about/dan.jpg" class="profile-img">
+                    <div class="about-text">
                         <p class="name-about"> Dan Haski </p>
-                        <p> </p>
-                        </div>
+                    </div>
                 </div>
                 <div  class="about about-team">
-                <img src="./img/about/sama-profile.jpg"  class="profile-img">
-                <div class="about-text">
-                <p class="name-about"> Samantha Kofler </p>
-                <p> </p>
-                </div>
-</div>
+                    <img src="./img/about/sama-profile.jpg"  class="profile-img">
+                    <div class="about-text">
+                        <p class="name-about"> Samantha Kofler </p>
+                    </div>
+            </div>
         </div>
     </section>
     `
